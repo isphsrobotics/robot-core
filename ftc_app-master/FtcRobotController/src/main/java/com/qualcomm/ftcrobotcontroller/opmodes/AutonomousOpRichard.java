74ffce534661d0edd693ee449f5b3e4e31aa524e
roflmaoshizmp <rsedivy@gmail.com>
2016-02-01 22:21:09 +0100

    Camera function code added & comments
    Most of the usage code completed for the camera. I still need to figure
    out how to pass it that bloody SurfaceHolder object, otherwise it
    should work and we can start implementing it in the rest of the code.
    
    In another news, more comments for my autonomous.

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousOpRichard.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousOpRichard.java
@@ -2,6 +2,8 @@
 
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.view.SurfaceHolder;
 
@@ -29,6 +31,9 @@ public class AutonomousOpRichard extends LinearOpMode {
     DcMotor tapeMotor;
     Servo leftServo;
     Servo tapeServo;
+
+    Bitmap image;
+
 
     /*
        * Code to run when the op mode is first enabled goes here
@@ -76,11 +81,14 @@ public class AutonomousOpRichard extends LinearOpMode {
         motorLeft.setPower(0.0);
         motorRight.setPower(0.0);
 
+        // wait a bit
+
         t = System.currentTimeMillis() + 1000;
         while (t > System.currentTimeMillis()){
 
         }
 
+        // turn left
 
         t = System.currentTimeMillis() + 1000;
         motorLeft.setPower(-1.0);
@@ -92,6 +100,8 @@ public class AutonomousOpRichard extends LinearOpMode {
 
         motorLeft.setPower(0.0);
         motorRight.setPower(0.0);
+
+        // move forward again
 
         t = System.currentTimeMillis() + 10000;
         motorLeft.setPower(1.0);
@@ -104,6 +114,8 @@ public class AutonomousOpRichard extends LinearOpMode {
         motorLeft.setPower(0.0);
         motorRight.setPower(0.0);
 
+        // turn left again
+
         t = System.currentTimeMillis() + 1000;
         motorLeft.setPower(-1.0);
         motorRight.setPower(1.0);
@@ -114,6 +126,8 @@ public class AutonomousOpRichard extends LinearOpMode {
 
         motorLeft.setPower(0.0);
         motorRight.setPower(0.0);
+
+        // move forward again
 
         t = System.currentTimeMillis() + 1000;
         motorLeft.setPower(1.0);
@@ -127,24 +141,110 @@ public class AutonomousOpRichard extends LinearOpMode {
         motorRight.setPower(0.0);
     }
 
+
+
+
+
     public int getCameraColor(SurfaceHolder s){
+
+        /*
+        METHOD TO FIND WHAT BEACON COLOR CAMERA IS LOOKING AT CURRENTLY
+        
+        This is done by taking a photo and analyzing the average color of the central 30% of the bitmap.
+
+        You have to pass a separate surface holder you have to make in the main activity.
+        Still haven't figured out how I'm going to do it.
+
+        This method returns the following values:
+        -1 - Something went wrong
+        0 - Neither color was decisively detected
+        1 - Red was detected
+        2 - Blue was detected
+
+    */
 
         // Attempt at using android camera libraries as sensors.
 
+        // Open camera
         Camera c = Camera.open();
 
+
+        // Get height and width of camera
+        int height = c.getParameters().getPictureSize().height;
+        int width = c.getParameters().getPictureSize().width;
+
         try {
+            // Have to set a preview display somewhere, still figuring this out
             c.setPreviewDisplay(s);
             c.startPreview();
+
+            // Taking a picture: Using a callback, which turns this into a BufferedImage, which is a global var
             c.takePicture(null, null, new Camera.PictureCallback() {
                 @Override
                 public void onPictureTaken(byte[] data, Camera camera) {
-
+                    image = BitmapFactory.decodeByteArray(data,0,data.length);
                 }
             });
-        }catch(IOException e){}
 
-        return 0;
+            // If for some reason camera fails to fire, return -1
+            // c.release() allows camera to be used by other apps after this (Important!)
+            if(image == null){
+                c.release();
+                return -1;
+            }
+
+
+            // Taking the middle 30% of the image (can be changed by changing the denominator)
+
+            int h1 = height/3;
+            int h2 = height - h1;
+
+            int w1 = width/3;
+            int w2 = width - w1;
+
+            // Getting the average of the pixel colours in this central region
+            // Note: There may be some loss of precision, mainly for the blue value (due to int division)
+
+            int val = 0;
+            int count = 0;
+
+            for(int i = w1; i<w2; i++){
+                for (int j =h1;j<h2;j++){
+                    val += image.getPixel(i,j);
+                    count++;
+                }
+            }
+
+            int p = val/count;
+
+            // Converting the color value into three separate RGB values
+          
+            int R = (p & 0xff0000) >> 16;
+            int G = (p & 0xff00) >> 8;
+            int B = p & 0xff;
+
+            // If the Red value is sufficiently high, and other values relatively low (i.e. it's red, and not white)
+            // TODO: Test these thresholds in a realistic environment
+            if(R > 180 && (B < 50 && G < 50)){
+                c.release();
+                return 1;
+                // RETURN 1 means it's red
+            } else if(B > 180 && (R < 50 && G < 50)){
+            // Same for Blue now
+            // TODO: Test threshold
+                c.release();
+                return 2;
+                // RETURN 2 means it's blue
+            } else {
+                c.release();
+                return 0;
+            }
+
+
+        }catch(IOException e){
+            c.release();
+            return -1;
+        }
 
     }
 
