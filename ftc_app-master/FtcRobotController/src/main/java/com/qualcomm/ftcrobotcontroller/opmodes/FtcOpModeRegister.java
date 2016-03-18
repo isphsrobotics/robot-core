a41af1a7154bf024fa9131350f6208dc8bd5be5f
Alex Balgavy <a.balgavy@gmail.com>
2016-03-18 18:31:10 +0100

    Removed unnecessary opmodes from register
    Removed Danny’s Auto, R, Linear Component Test, ServoTeleOp.
    Reason: currently not in use & unnecessary.

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -57,9 +57,5 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("TeleOp", TeleOp.class);
     manager.register("AUTONOMOUS RED",AutonomousRed.class);
     manager.register("AUTONOMOUS BLU", AutonomousBlu.class);
-    manager.register("Danny's Auto", AutonomousBlu.class);
-    manager.register("R", AutonomousOpRichard.class);
-    manager.register("Linear Component Test",LinearComponentTest.class);
-    manager.register("ServoTeleOp", ServoTestTeleOp.class);
   }
 }
