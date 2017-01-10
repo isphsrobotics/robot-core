2a2d93bcaea51080dbda1c2a8d95ac5c8f27e609
DanielHamill <danielhamill@Daniels-MacBook-Air.local>
2017-01-10 15:55:09 +0100

    trying autonomous stuff

--- ftc_app-2.2.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ ftc_app-2.2.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -93,7 +93,7 @@ public class TestAutonomous extends LinearOpMode {
         runtime.reset();
 
         // run until the end of the match (driver presses `)
-        while (opModeIsActive()) {
+        if (opModeIsActive()) {
             telemetry.addData("Status", "Run Time: " + runtime.toString());
 
 
